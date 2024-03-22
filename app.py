c2dcbff946f6a5b3f6ec2e5acf6793d5783d8205
Priyanka Naskar <128339382+Priyankanaskar@users.noreply.github.com>
2024-03-22 10:49:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from shiny.express import input, ui
 from shinywidgets import render_plotly
 from palmerpenguins import load_penguins
 import shinyswatch
-import seaborn as sns
-from faicons import icon_svg
-from htmltools import css
 
-# Theme
+
+# Theme------------------------------------------------------------------------------------------------------------------------------------------------------------
 shinyswatch. theme.darkly()
+
 # This package provides the Palmer Penguins dataset----------------------------------------------------------------------------------------------
 
 from shiny import reactive, render, req
 import seaborn as sns
 import pandas as pd
 import palmerpenguins
+
 # Use the built-in function to load the Palmer Penguins dataset-----------------------------------------------------------------------------------------------
 import seaborn as sns
 penguins = sns.load_dataset("penguins")
@@ -62,6 +62,7 @@ with ui.sidebar(open="open"):
         selected=["Torgersen", "Biscoe", "Dream"],
         inline=False,
     )
+    
 # Add a horizontal rule to the sidebar----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     ui.hr()
     
@@ -81,6 +82,7 @@ with ui.accordion(id="acc", open="closed"):
         @render.data_frame
         def penguin_datagrid():
             return render.DataGrid( filtered_data())
+            
 # Plot Charts----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ui.input_select("x", "Variable:",
                 choices=["bill_length_mm", "bill_depth_mm"])
@@ -88,7 +90,7 @@ ui.input_select("dist", "Distribution:", choices=["hist", "kde"])
 ui.input_checkbox("rug", "Show rug marks", value = False)
 
 
-## Column
+## Column--------------------------------------------------------------------------------------------------------------------------------------------
 
 
 @render.plot
@@ -142,24 +144,6 @@ app_ui = ui.page_fillable(
         length="2px"
     ),
 )
-
-# The contents of the first 'page' is a navset with two 'panels'.
-page1 = ui.navset_card_underline(
-    ui.nav_panel("Plot", ui.output_plot("hist")),
-    ui.nav_panel("Table", ui.output_data_frame("data")),
-    footer=ui.input_select(
-        "var", "Select variable", choices=["bill_length_mm", "body_mass_g"]
-    ),
-    title="Penguins data",
-)
-
-app_ui = ui.page_navbar(
-    ui.nav_spacer(),  # Push the navbar items to the right
-    ui.nav_panel("Page 1", page1),
-    ui.nav_panel("Page 2", "This is the second 'page'."),
-
-)
-
 
 #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # Add a reactive calculation to filter the data
